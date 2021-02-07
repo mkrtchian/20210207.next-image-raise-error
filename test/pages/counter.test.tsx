09d174b28ffa22b7adcac511317b48c372aac113
Roman Mkrtchian <mkrtchia@gmail.com>
2021-02-07 15:27:20 +0100

    test: try with act on expect

--- test/pages/counter.test.tsx
+++ test/pages/counter.test.tsx
@@ -1,16 +1,20 @@
-import { screen, render, fireEvent, waitFor } from '@testing-library/react'
+import { screen, render, fireEvent, act } from '@testing-library/react'
 import { Counter } from '../../pages/counter'
 
 describe('Counter', () => {
   it('Initial Value is 0', async () => {
     render(<Counter />)
-    await waitFor(() => expect(screen.getByText('0', { selector: 'p' })))
+    await act(async () => {
+      expect(screen.getByText('0', { selector: 'p' }))
+    })
   })
 
   it('Pressing `+` button increments the counter.', async () => {
     render(<Counter />)
     expect(screen.getByText('0', { selector: 'p' }))
     fireEvent.click(screen.getByText('+', { selector: 'button' }))
-    await waitFor(() => expect(screen.getByText('1', { selector: 'p' })))
+    await act(async () => {
+      expect(screen.getByText('1', { selector: 'p' }))
+    })
   })
 })
