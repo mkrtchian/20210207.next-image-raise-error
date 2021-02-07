125907cfc6eeff74af3691ace7f2d335c9ab5409
Roman Mkrtchian <mkrtchia@gmail.com>
2021-02-07 16:45:32 +0100

    test: try with an explicit wait time

--- test/pages/counter.test.tsx
+++ test/pages/counter.test.tsx
@@ -6,6 +6,7 @@ describe('Counter', () => {
     render(<Counter />)
     await act(async () => {
       expect(screen.getByText('0', { selector: 'p' }))
+      await new Promise((res) => setTimeout(res, 100))
     })
   })
 
@@ -15,6 +16,7 @@ describe('Counter', () => {
     fireEvent.click(screen.getByText('+', { selector: 'button' }))
     await act(async () => {
       expect(screen.getByText('1', { selector: 'p' }))
+      await new Promise((res) => setTimeout(res, 100))
     })
   })
 })
